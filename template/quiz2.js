e12fb2aaf3404db492923b99d5a5e3fd4bb7eb84
Luke Kljucaric <lek70@pitt.edu>
2016-03-28 22:26:55 -0400

    Functionality is done.
    Part 1 - Complete
    Part 2 - Request/generate button/set cookie done

--- template/quiz2.js
+++ template/quiz2.js
@@ -8,6 +8,9 @@
 	$click     = $('.click');
 	$submit       = $('.submit');
 	$timeout   = $('.timeout');
+	$title	   = $('.title');
+	$content   = $('.content');
+	var first = true;
 
 	$mouseover.on('mouseover', function() {
 		$this = $(this);
@@ -31,15 +34,42 @@
 		}
 	});
 
-	$
-
 	$(document).ready(function() {
 		setTimeout(function() {
-			console.log("test");
 			$timeout.fadeIn('slow');
 		}, 1000);
 	});
 
+	$title.on('click', function() {
+		$.ajax({
+			url: "http://www.mattbowytz.com/simple_api.json?data=quizData",
+			success: function(result) {
+				var data = result.data;
+
+				$content.empty();
+				$content.append('<div class="next"></div><div class="do"></div>');
+				$next = $('.next');
+				$doButtons = $('.do');
+
+				if(document.cookie != '')
+				{
+					var showCookie = document.cookie.split('=');
+					$next.html(showCookie[1]);
+				}
+
+				$doButtons.append($('<button>Change It</button>').click(function () {
+					$next.html('<h2>' + data[Math.floor(Math.random() * data.length)] + '</h2>');
+					if(first)
+					{
+						first = false;
+						$doButtons.append('<button>Keep It</button>').click(function () {
+							document.cookie = "val=" + $next.html();
+						});
+					}
+				}));
+			}
+		});
+	});
 
 
 })(jQuery);
